e09f1bfcc535f030aa6449a3c6911decb10716ae
Busola Tom <busolatom@gmail.com>
2024-06-28 21:21:34 +0000

    Router created and exported

---
+++ routes/chat_route.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+// create router
+const chatRouter = Router();
+
+// define routes
+chatRouter.post('/chat', )
+
+chatRouter.get('/chat', )
+
+chatRouter.get('/chat/:id', )
+
+chatRouter.patch('/chat/:id', )
+
+chatRouter.delete('/chat/:id', )
+
+// export router 
+export default chatRouter;
